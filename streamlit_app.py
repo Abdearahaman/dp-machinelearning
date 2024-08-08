7589b5d6acba460411937aa65dd658aeda04b9cd
Abdearahaman <164544402+Abdearahaman@users.noreply.github.com>
2024-08-08 17:46:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with st.sidebar:
           'bill_depth_mm':bill_depth_mm,
           'flipper_length_mm':flipper_length_mm,
           'body_mass_g':body_mass_g,
-          'gender':sex}
+          'sex':sex}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins =pd.concat([input_df, X], axis=0)
 with st.expander('Input features'):
@@ -46,5 +46,5 @@ with st.expander('Input features'):
   st.write('**Combined penguins data**')
   input_penguins
 # Encode
-encode = ['island','gender',]
+encode = ['island','sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
